4ba16db09cef541369c0e1c94081334641904112
Daniil Kogtev <oclaw@joom.com>
2023-12-08 20:06:29 +0300

    day8 improvements

--- day8/main.zig
+++ day8/main.zig
@@ -11,9 +11,9 @@ const taskData = @embedFile("task8.txt");
 // for space economy we store 3 letters in 15 bits
 const bits_for_byte = 5;
 
-fn encode_vertex(vertex: []const u8) usize {
+fn encode_vertex(vertex_str: []const u8) usize {
     var result: usize = 0;
-    inline for (vertex[0..3]) |c| {
+    inline for (vertex_str[0..3]) |c| {
         assert(c >= 'A' and c <= 'Z');
         result = (result << bits_for_byte) | (c - 'A');
     }
@@ -22,46 +22,59 @@ fn encode_vertex(vertex: []const u8) usize {
 
 const MaxCode = encode_vertex("ZZZ") + 1; // about 26K array with 15K possible vertexes
 
-const node = struct {
+const vertex = struct {
     left: usize,
     right: usize,
-    is_set: bool,
 };
 
 // graph is stored as linear array with 15 bit code as index
-const tree = struct {
-    nodes: [MaxCode]node,
+const graph = struct {
+    vertexes: [MaxCode]vertex,
+    bitmap: std.bit_set.StaticBitSet(MaxCode) = std.bit_set.StaticBitSet(MaxCode).initEmpty(),
 
-    pub fn init() tree {
-        var t = tree{ .nodes = undefined };
-        @memset(&t.nodes, std.mem.zeroes(node));
-        return t;
+    pub fn init() graph {
+        var g = graph{ .vertexes = undefined };
+        @memset(&g.vertexes, std.mem.zeroes(vertex));
+        return g;
+    }
+
+    pub fn set_vertex(self: *graph, code: usize, v: vertex) void {
+        self.vertexes[code] = v;
+        self.bitmap.setValue(code, true);
+    }
+
+    pub fn is_set(self: *const graph, code: usize) bool {
+        return self.bitmap.isSet(code);
+    }
+
+    pub fn get_next(self: *const graph, source: usize, move: u8) usize {
+        return switch (move) {
+            'L' => self.vertexes[source].left,
+            'R' => self.vertexes[source].right,
+            else => unreachable,
+        };
     }
 };
 
-fn solve_part1(t: *const tree, moves: []const u8) !usize {
+fn solve_part1(g: *const graph, moves: []const u8) !usize {
     var cur = encode_vertex("AAA");
     const end = encode_vertex("ZZZ");
     var movecount: u64 = 0;
-    if (!t.nodes[cur].is_set or !t.nodes[end].is_set) {
+    if (!g.is_set(cur) or !g.is_set(end)) {
         std.debug.print("no solution for part 1\n", .{});
         return Error.NoSolutions;
     }
 
     while (cur != end) {
         var move = moves[movecount % moves.len];
-        cur = switch (move) {
-            'L' => t.nodes[cur].left,
-            'R' => t.nodes[cur].right,
-            else => unreachable,
-        };
+        cur = g.get_next(cur, move);
         movecount += 1;
     }
 
     return movecount;
 }
 
-fn solve_part2(t: *const tree, moves: []const u8) usize {
+fn solve_part2(g: *const graph, moves: []const u8) usize {
     // vertex start criteria: (code & 0b11111) == 0  (encoded 'A')
     // vertex end critertia:  (code & 0b11111) == 25 (encoded 'Z')
     const aux = struct {
@@ -78,8 +91,8 @@ fn solve_part2(t: *const tree, moves: []const u8) usize {
     var cur_buf = [_]usize{0} ** MaxCode;
     var steps_to_end_buf = [_]usize{0} ** MaxCode;
     var starts_count: usize = 0;
-    for (t.nodes, 0..) |_node, i| {
-        if (!_node.is_set) {
+    for (0..g.vertexes.len) |i| {
+        if (!g.is_set(i)) {
             continue;
         }
 
@@ -97,11 +110,8 @@ fn solve_part2(t: *const tree, moves: []const u8) usize {
         var temp = start;
         var movecount: usize = 0;
         while (!aux.is_end(temp)) {
-            temp = switch (moves[movecount % moves.len]) {
-                'L' => t.nodes[temp].left,
-                'R' => t.nodes[temp].right,
-                else => unreachable,
-            };
+            var move = moves[movecount % moves.len];
+            temp = g.get_next(temp, move);
             movecount += 1;
         }
         steps_to_end_buf[i] = movecount;
@@ -112,11 +122,9 @@ fn solve_part2(t: *const tree, moves: []const u8) usize {
     var lcm: u64 = 0;
     outer: while (true) {
         lcm += longest_path;
-        for (steps_to_end) |divisor| {
-            if (lcm % divisor != 0) {
+        for (steps_to_end) |divisor|
+            if (lcm % divisor != 0)
                 continue :outer;
-            }
-        }
         break;
     }
 
@@ -124,12 +132,12 @@ fn solve_part2(t: *const tree, moves: []const u8) usize {
 }
 
 const task = struct {
-    tree: tree,
+    graph: graph,
     moves: []const u8,
 };
 
 fn parse_task(data: []const u8) task {
-    var t = tree.init();
+    var g = graph.init();
     var iter = std.mem.splitScalar(u8, data, '\n');
     var moves = iter.first();
     var count: usize = 0;
@@ -141,41 +149,41 @@ fn parse_task(data: []const u8) task {
         var code = encode_vertex(line[0..3]);
         var left = encode_vertex(line[7..10]);
         var right = encode_vertex(line[12..15]);
-        t.nodes[code] = node{ .left = left, .right = right, .is_set = true };
+        g.set_vertex(code, vertex{ .left = left, .right = right });
         count += 1;
     }
-    return task{ .tree = t, .moves = moves };
+    return task{ .graph = g, .moves = moves };
 }
 
 pub fn main() !void {
     var t = parse_task(taskData);
 
-    std.debug.print("part 1: {any}\n", .{solve_part1(&t.tree, t.moves)});
-    std.debug.print("part 2: {}\n", .{solve_part2(&t.tree, t.moves)});
+    std.debug.print("part 1: {any}\n", .{solve_part1(&t.graph, t.moves)});
+    std.debug.print("part 2: {}\n", .{solve_part2(&t.graph, t.moves)});
 }
 
 const expect = std.testing.expect;
 
 test "sample 1" {
     var t = parse_task(sample1);
-    try expect(try solve_part1(&t.tree, t.moves) == 2);
-    try expect(solve_part2(&t.tree, t.moves) == 2);
+    try expect(try solve_part1(&t.graph, t.moves) == 2);
+    try expect(solve_part2(&t.graph, t.moves) == 2);
 }
 
 test "sample 2" {
     var t = parse_task(sample2);
-    try expect(try solve_part1(&t.tree, t.moves) == 6);
-    try expect(solve_part2(&t.tree, t.moves) == 6);
+    try expect(try solve_part1(&t.graph, t.moves) == 6);
+    try expect(solve_part2(&t.graph, t.moves) == 6);
 }
 
 test "sample 3" {
     var t = parse_task(sample3);
-    try expect(solve_part1(&t.tree, t.moves) == error.NoSolutions);
-    try expect(solve_part2(&t.tree, t.moves) == 6);
+    try expect(solve_part1(&t.graph, t.moves) == error.NoSolutions);
+    try expect(solve_part2(&t.graph, t.moves) == 6);
 }
 
 test "task" {
     var t = parse_task(taskData);
-    try expect(try solve_part1(&t.tree, t.moves) == 16697);
-    try expect(solve_part2(&t.tree, t.moves) == 10668805667831);
+    try expect(try solve_part1(&t.graph, t.moves) == 16697);
+    try expect(solve_part2(&t.graph, t.moves) == 10668805667831);
 }
